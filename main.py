8099a52abc82541908dab2461c8b41aeeaeec712
Vimal Shah <shahvimal1302@gmail.com>
2020-03-07 10:44:28 +0530

    Update

--- main.py
+++ main.py
@@ -12,4 +12,4 @@ def home():
 if __name__ == "__main__":
     from random_forest_classification import ps, acc, rcs
     # app.run(debug=True)
-    app.run(host=’0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
