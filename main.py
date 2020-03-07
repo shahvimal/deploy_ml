68d23860654e25410a5a3caab5fa9f1c67a2c764
Vimal Shah <shahvimal1302@gmail.com>
2020-03-07 10:52:54 +0530

    hi

--- main.py
+++ main.py
@@ -8,7 +8,12 @@ cors = CORS(app)
 def home():
     # return render_template("home.html", ps = ps, acc = acc, rcs = rcs)
     return jsonify(ps = ps, acc = acc, rcs = rcs, test="../static/testplot.png")
-    
+
+@app.route("/hello")
+def home():
+    # return render_template("home.html", ps = ps, acc = acc, rcs = rcs)
+    return ('hello')
+  
 if __name__ == "__main__":
     from random_forest_classification import ps, acc, rcs
     # app.run(debug=True)
